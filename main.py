23f0990f1ae84cd0e7ffe5b99def82fba2f86bbb
Walle <valek.kim2000@gmail.com>
2021-10-01 11:14:57 +0300

    Plus and minus ready.

--- main.py
+++ main.py
@@ -1,46 +1,42 @@
-def solve(strs):
-    out = 0
+def getsign(sign, number):
+    return number * (-1) if sign == False else number
 
-    def getsign(sign, number):
-        return number * (-1) if sign == False else number
 
-    def find_digit(j, s_searching):
-        processing_number = ''
-        while s_searching.isdigit() == True:
-            processing_number += strs[j]
-            if j < len(s_searching) - 1:
-                j += 1
-            else:
-                break
-        return processing_number, j
+def find_digit(j, s_searching):
+    processing_number = ''
+    while s_searching[j].isdigit() == True:
+        processing_number += s_searching[j]
+        if j < len(s_searching) - 1:
+            j += 1
+        else:
+            break
+    return j, int(processing_number)
+
+
+def plus_minus(strs_without_multipl):
+    i = 0
+    sum = 0
+    processed_number = ''
+    sign = True
+
+    while i < len(strs_without_multipl)-1:
+        if strs_without_multipl[i] == '-':
+            sign = False
+            i += 1
+        elif strs_without_multipl[i] == '+':
+            sign = True
+            i += 1
+
+        i, processed_number = find_digit(i, strs_without_multipl)
+
+        if sum != '':
+            sum += getsign(sign, processed_number)
+            processed_number = ''
+    return sum
 
     def multiplication(not_readystr):
         i = not_readystr.find('*')
 
-
-    def sum(strs_without_multipl):
-        i = 0
-        sum = 0
-        processed_number = ''
-        sign = True
-
-        while i < len(strs_without_multipl)-1:
-            if strs_without_multipl[i] == '-':
-                sign = False
-                i += 1
-            elif strs_without_multipl[i] == '+':
-                sign = True
-                i += 1
-
-            processed_number, i = find_digit(i, strs_without_multipl)
-
-            if sum != '':
-                sum += getsign(sign, int(processed_number))
-                processed_number = ''
-
-    out = sum(strs)
-
-    return out
 
 def check_problems(s):
     b = True
@@ -58,6 +54,15 @@ def check_problems(s):
         else:
             b = False
     return b
+
+def solve(strs):
+
+    out = plus_minus(strs)
+
+# Multiplication coming soon
+#     LOL
+
+    return out
 
 s = str(input())
 
